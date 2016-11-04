caa9381a699aa62857701988b206cc32e0e718d5
Nat Wilson <njwilson23@gmail.com>
2016-11-04 16:38:19 -0700

    update the way data is requested
    temporal resampling now happens server-side to reduce bandwidth
    
    data is requested using the following template:
    
        /data/{filename}.{extension}[?minute=N][?raw=B]
    
    where filename is something like `mc1` or `aq3`, extension is one of
    `json` or `csv`, N is an integer greater than 60, and raw is 0 (return
    filtered data) or 1 (return raw data)
    
    filtering removes the data where the MC4 salinity was flaky
    
    temporal resampling is a windowed mean (previously it was just every
    12th data point)

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
 """
 Server for itm5live
 """
+import os
+import datetime
 import json
-import database
 import flask
+import pandas
+import dateutil.parser
+import database
 
 app = flask.Flask("itm5live", static_url_path="")
 
@@ -31,45 +35,44 @@ def css(filename):
 def js(filename):
     return flask.send_from_directory("js", filename)
 
-@app.route("/data/<path:filename>")
-def send_data(filename):
+def get_data(filename):
     files = {
-        "mc1.json" : {
+        "mc1" : {
             "table": "itm5",
             "fields": ["date AT TIME ZONE 'UTC'", "mc1temperature", "mc1salinity", "mc1pressure"],
             "retfields": ["date", "temperature", "salinity", "pressure"]
         },
-        "mc2.json": {
+        "mc2": {
             "table": "itm5",
             "fields": ["date AT TIME ZONE 'UTC'", "mc2temperature", "mc2salinity", "mc2pressure"],
             "retfields": ["date", "temperature", "salinity", "pressure"]
         },
-        "mc3.json": {
+        "mc3": {
             "table": "itm5",
             "fields": ["date AT TIME ZONE 'UTC'", "mc3temperature", "mc3salinity", "mc3pressure"],
             "retfields": ["date", "temperature", "salinity", "pressure"]
         },
-        "mc4.json": {
+        "mc4": {
             "table": "itm5",
             "fields": ["date AT TIME ZONE 'UTC'", "mc4temperature", "mc4salinity", "mc4pressure"],
             "retfields": ["date", "temperature", "salinity", "pressure"]
         },
-        "aq1.json" : {
+        "aq1" : {
             "table": "itm5",
             "fields": ["date AT TIME ZONE 'UTC'", "ad1temperature", "ad1pressure", "ad1north", "ad1east", "ad1up"],
             "retfields": ["date", "temperature", "pressure", "north", "east", "up"]
         },
-        "aq2.json": {
+        "aq2": {
             "table": "itm5",
             "fields": ["date AT TIME ZONE 'UTC'", "ad2temperature", "ad2pressure", "ad2north", "ad2east", "ad2up"],
             "retfields": ["date", "temperature", "pressure", "north", "east", "up"]
         },
-        "aq3.json": {
+        "aq3": {
             "table": "itm5",
             "fields": ["date AT TIME ZONE 'UTC'", "ad3temperature", "ad3pressure", "ad3north", "ad3east", "ad3up"],
             "retfields": ["date", "temperature", "pressure", "north", "east", "up"]
         },
-        "aq4.json": {
+        "aq4": {
             "table": "itm5",
             "fields": ["date AT TIME ZONE 'UTC'", "ad4temperature", "ad4pressure", "ad4north", "ad4east", "ad4up"],
             "retfields": ["date", "temperature", "pressure", "north", "east", "up"]
@@ -85,8 +88,55 @@ def send_data(filename):
     if params is None:
         flask.abort(404)
 
-    return json.dumps(database.extract(params["table"], params["fields"],
-                                       retfields=params.get("retfields", None)))
+    return database.extract(params["table"], params["fields"], retfields=params.get("retfields", None))
+
+@app.route("/raw/<path:filename>")
+def send_raw_data(filename):
+    return json.dumps(get_data(filename))
+
+def add_utc(d):
+    return datetime.datetime(d.year, d.month, d.day, d.hour, d.minute, d.second,
+                             tzinfo=datetime.timezone.utc)
+
+@app.route("/data/<path:filename>")
+def send_data(filename):
+    try:
+        timeResolution = int(flask.request.args.get("minute", 60))
+    except ValueError:
+        timeResolution = 60
+
+    try:
+        rawData = bool(flask.request.args.get("raw", 0))
+    except ValueError:
+        rawData = False
+
+    # load data from the database and parse a date index
+    d = get_data(os.path.splitext(filename)[0])
+    dateIndex = [add_utc(dateutil.parser.parse(a)) for a in d["date"]]
+    referenceTime = datetime.datetime(2000, 1, 1, 0, 0, 0, tzinfo=datetime.timezone.utc)
+    d["timestamp"] = [(d-referenceTime).total_seconds() for d in dateIndex]
+    del d["date"]
+
+    df = pandas.DataFrame(data=d, index=dateIndex)
+
+    # filter out bad MC4 data
+    if not rawData:
+        if filename.startswith("mc4"):
+            df.salinity = df.salinity.apply(lambda x: x if x > 34.72 else None)
+
+        #df.dropna(how="all", inplace=True)
+
+    # resample by time
+    if timeResolution > 60:
+        df = df.resample("{0:d}T".format(round(timeResolution))).mean()
+
+    if filename.endswith(".json"):
+        return json.dumps(df.to_dict(orient="list")).replace("NaN", "null")
+    elif filename.endswith(".csv"):
+        return df.to_csv()
+    else:
+        flask.abort(404)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
